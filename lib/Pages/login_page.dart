44e3433f75f886da8b2fd054e19b3f264416aed9
Nitesh Kumar Sharma <135590704+Niteshsharma831@users.noreply.github.com>
2024-07-13 14:41:18 +0530

    images

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -4,13 +4,8 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 40, color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+        child: Column(
+      children: [Image.asset("asset/images/Login_image.png")],
+    ));
   }
 }
